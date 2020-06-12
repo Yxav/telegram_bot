b6da0de6d4110c6d4597c5136f55e18aaac56ea1
Yuri <xavier.ysx@gmail.com>
2020-06-12 15:18:37 -0300

    Function of mention the new user on the chat group

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Telegraf } = require('telegraf')
+
+BOT_TOKEN = ''
+const bot = new Telegraf(BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Hey, Im working'))
+
+
+
+bot.on('new_chat_members', async(ctx) => {
+    var id = ctx.update.message.new_chat_member.id
+    var user = await ctx.getChatMember(id)
+    if (user.user.username) {
+        ctx.reply(`Welcome @${user.user.username}`)
+    } else if (!user.user.username) {
+        if (!user.user.last_name) {
+            ctx.reply(`Welcome ${user.user.first_name}`)
+        } else {
+            ctx.reply(`Welcome ${user.user.first_name} ${user.user.last_name}`)
+        }
+    }
+})
+
+
+bot.launch()
