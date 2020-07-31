d794a1e1d8e6f6a0ed5f054a195e9c588ed12482
Yuri <yuri_dsx_@hotmail.com>
2020-07-31 10:48:32 -0300

    refactoring

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ bot.start((ctx) => ctx.reply('Hey, Im working'))
 bot.on('new_chat_members', async(ctx) => {
     var id = ctx.update.message.new_chat_member.id
     var user = await ctx.getChatMember(id)
-    if (user.user.username) {
-        ctx.reply(`Welcome @${user.user.username}`)
-    } else if (!user.user.username) {
-        if (!user.user.last_name) {
+    if (!user.user.username) {
+        if (!user.user.lastname) {
             ctx.reply(`Welcome ${user.user.first_name}`)
         } else {
             ctx.reply(`Welcome ${user.user.first_name} ${user.user.last_name}`)
         }
     }
-})
+
+    ctx.reply(`Welcome ${user.user.username}`)
+
+
+});
 
 
 bot.launch()
